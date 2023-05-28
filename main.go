54d79d14e7be840283484975c6a4ad2966f49cb2
marahmanjs <imarahmandev@gmail.com>
2023-05-28 21:33:17 +0700

    basic server

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
 
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+
+	fmt.Println("Starting server at Port 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
